e9b0d0f6e241388311df5c39a666906707ec7bfa
Priyanshu Kumar <connect.priyanshu8271@gmail.com>
2026-04-03 12:19:44 +0530

    gomod: add vcs_url PURL qualifier for local modules
    The gomod backend was not including the vcs_url qualifier on PURLs for modules sourced from the local git repository. This affected the main module, workspace modules, and local replacement modules.
    
    Other backends (cargo, bundler, pip, npm) already include vcs_url for locally-sourced components.
    
    Resolves: #1434
    Signed-off-by: Priyanshu Kumar <connect.priyanshu8271@gmail.com>

--- tests/unit/package_managers/gomod/test_main.py
+++ tests/unit/package_managers/gomod/test_main.py
@@ -1577,7 +1577,7 @@ def test_missing_gomod_file(
             [
                 Component(
                     name="github.com/my-org/my-repo",
-                    purl="pkg:golang/github.com/my-org/my-repo@v1.0.0?type=module",
+                    purl="pkg:golang/github.com/my-org/my-repo@v1.0.0?type=module&vcs_url=git%2Bhttps://github.com/my-org/my-repo.git%40abc123",
                     version="v1.0.0",
                 ),
                 Component(
@@ -1595,7 +1595,7 @@ def test_missing_gomod_file(
                 ),
                 Component(
                     name="github.com/my-org/my-repo",
-                    purl="pkg:golang/github.com/my-org/my-repo@v1.0.0?type=package",
+                    purl="pkg:golang/github.com/my-org/my-repo@v1.0.0?type=package&vcs_url=git%2Bhttps://github.com/my-org/my-repo.git%40abc123",
                     version="v1.0.0",
                 ),
                 Component(
@@ -1639,12 +1639,12 @@ def test_missing_gomod_file(
             [
                 Component(
                     name="github.com/my-org/my-repo",
-                    purl="pkg:golang/github.com/my-org/my-repo@v1.0.0?type=module",
+                    purl="pkg:golang/github.com/my-org/my-repo@v1.0.0?type=module&vcs_url=git%2Bhttps://github.com/my-org/my-repo.git%40abc123",
                     version="v1.0.0",
                 ),
                 Component(
                     name="github.com/my-org/my-repo/path",
-                    purl="pkg:golang/github.com/my-org/my-repo/path@v1.0.0?type=module",
+                    purl="pkg:golang/github.com/my-org/my-repo/path@v1.0.0?type=module&vcs_url=git%2Bhttps://github.com/my-org/my-repo.git%40abc123",
                     version="v1.0.0",
                 ),
                 Component(
@@ -1674,7 +1674,9 @@ def test_missing_gomod_file(
 @mock.patch("hermeto.core.package_managers.gomod.main._get_go_work_path")
 @mock.patch("hermeto.core.package_managers.gomod.main._vendor_changed")
 @mock.patch("hermeto.core.package_managers.gomod.main.create_backend_annotation")
+@mock.patch("hermeto.core.package_managers.gomod.main.get_repo_id")
 def test_fetch_gomod_source(
+    mock_get_repo_id: mock.Mock,
     mock_create_annotation: mock.Mock,
     mock_vendor_changed: mock.Mock,
     mock_get_go_work_path: mock.Mock,
@@ -1715,6 +1717,12 @@ def test_fetch_gomod_source(
     mock_find_missing_gomod_files.return_value = []
     mock_get_repository_name.return_value = "github.com/my-org/my-repo"
     mock_vendor_changed.return_value = False
+
+    mock_repo_id = mock.Mock()
+    mock_repo_id.as_vcs_url_qualifier.return_value = (
+        "git+https://github.com/my-org/my-repo.git@abc123"
+    )
+    mock_get_repo_id.return_value = mock_repo_id
 
     mock_tmp_dir.name = "tmpdir"
     mock_tmp_dir.return_value.__enter__.return_value = mock_tmp_dir
